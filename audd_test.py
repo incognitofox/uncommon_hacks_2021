56237ed374905bf954313a9d8946c0c9f604b6cd
NameZach <rothwellz@uchicago.edu>
2021-04-03 20:35:19 -0500

    works for any music

--- audd_test.py
+++ audd_test.py
@@ -2,6 +2,10 @@ import keys
 import json
 import requests
 import sys
+import os
+
+def url_to_mp3 (url):
+    os.system("youtube-dl --extract-audio --audio-format mp3 --output \"noise.mp3\" " + url + " -k")
 
 def get_audio_info(directory, file = False, url = False):
     result = None
@@ -25,7 +29,18 @@ def get_audio_info(directory, file = False, url = False):
     tmp = result.json()
     print("title: " + str(tmp["result"]["title"]))
     print("artist: " + str(tmp["result"]["artist"]))
+    return (str(tmp["result"]["title"]), str(tmp["result"]["artist"]))
+#    print("title: " + str(tmp["result"]["title"]))
+#    print("artist: " + str(tmp["result"]["artist"]))
    
-args = sys.argv
-get_audio_info(args[1], file = args[2], url = not args[2])
+def url_to_audio (url):
+    url_to_mp3(url)
+    title, artist = get_audio_info ("noise.mp3.mp3", file = True)
+    is_rick = (title == "Never Gonna Give You Up" and artist == "Rick Astley")
+    if is_rick:
+        print ("Ha! Get pranked! You've been Ricked Rolled")
+    else:
+        print ("This is actually " + title + " by " + artist)
+    os.system ("rm -rf noise*")
 
+url_to_audio(sys.argv[1])
