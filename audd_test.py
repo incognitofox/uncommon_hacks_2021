f976078f0ba6c120b2ae06f58719498c9d05ef89
incognitofox <wwill2011@gmail.com>
2021-04-03 19:35:35 -0500

    cleaned audd_test output

--- audd_test.py
+++ audd_test.py
@@ -1,13 +1,17 @@
 import keys
+import json
 import requests
 
 print(keys.audd_key)
 files = {
-    'file' : open("test.mp3", "rb")
+    'file' : open("hidden_test.mp3", "rb")
 }
 data = {
     'api_token': keys.audd_key,
+    'url': "https://audd.tech/example1.mp3", 
     'return': 'apple_music,spotify',
 }
-result = requests.post('https://api.audd.io/', data=data, files=files)
-print(result.text)
+result = requests.post('https://api.audd.io/', data=data)
+tmp = result.json()
+print("title: " + str(tmp["result"]["title"]))
+print("artist: " + str(tmp["result"]["artist"]))
