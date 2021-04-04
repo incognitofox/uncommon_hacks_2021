5a02552e1de1c8ed36aa3cd0fe06b37a32f66df4
NameZach <rothwellz@uchicago.edu>
2021-04-03 20:57:01 -0500

    can check if rick roll

--- audd_test.py
+++ audd_test.py
@@ -25,20 +25,24 @@ def get_audio_info(directory, file = False, url = False):
             'return': 'apple_music,spotify',
         }
         result = requests.post('https://api.audd.io/', data=data)
-         
     tmp = result.json()
-    print("title: " + str(tmp["result"]["title"]))
-    print("artist: " + str(tmp["result"]["artist"]))
-    return (str(tmp["result"]["title"]), str(tmp["result"]["artist"]))
+    if (tmp["result"] == None):
+        return (None, None)
 #    print("title: " + str(tmp["result"]["title"]))
 #    print("artist: " + str(tmp["result"]["artist"]))
+    return (str(tmp["result"]["title"]), str(tmp["result"]["artist"]))
    
 def url_to_audio (url):
     if "youtube" not in url:
         print ("This is not a youtube video")
+        os.system ("rm -rf noise*")
         return
     url_to_mp3(url)
     title, artist = get_audio_info ("noise.mp3.mp3", file = True)
+    if (title == None and artist == None):
+        print ("This is not a song")
+        os.system ("rm -rf noise*")
+        return
     is_rick = (title == "Never Gonna Give You Up" and artist == "Rick Astley")
     if is_rick:
         print ("Ha! Get pranked! You've been Ricked Rolled")
